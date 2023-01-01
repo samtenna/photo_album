a9f4825b08c11a0165f7daaab492d23df298ac44
Samuel Tennant <samueltennant@hotmail.com>
2023-01-01 20:02:41 +0000

    Added pretest

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const app = require('./server');
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`🖥️  Server listening on port: ${PORT}`);
+});
--- src/server.js
+++ src/server.js
@@ -1,18 +1,12 @@
 const path = require('path');
 const express = require('express');
 
-const PORT = 8080;
-
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (_, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
-});
-
-app.listen(PORT, () => {
-  console.log(`🖥️ Server listening on port: ${PORT}`);
 });
 
 module.exports = app;
